3edea9aa7a59ef67b0a843af5610c63728c011d1
Muhammad Usman <mu08697@gmail.com>
2020-12-04 21:05:39 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './mycomponents/first/first.component';
+import { SliderComponent } from './slider/slider.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SliderComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
